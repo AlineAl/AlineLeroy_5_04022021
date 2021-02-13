35e3d2e6d582384d5a8c967710b9df366955d5b6
Aline Leroy <aline.leroy.27@gmail.com>
2021-02-13 02:43:23 +0100

    page view des produits avec JavaScript, puis html, css de la page

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-let resultatsAPI;
+const nameArticle = document.querySelectorAll('.name-article');
+const priceArticle = document.querySelectorAll('.price-article');
+const descriptionArticle = document.querySelectorAll('.description-article');
+const imgArticle = document.querySelectorAll('.img-article');
 
-function appelAPI(id, name, price, description, image) {
 
-    fetch(`http://localhost:3000/api/cameras`)
-    .then((reponse) => {
-        return reponse.json();
-    })
-    .then((data) => {
-        resultatsAPI = data;
-        console.log(data); 
-    })
-}
+fetch(`http://localhost:3000/api/cameras`)
+.then((reponse) => {
+    return reponse.json()
+})
+.then((data) => {
+    // console.log(data);
+
+    for(let i = 0; i < 5; i++) {
+        imgArticle[i].src = `./images/vcam_${i + 1}.jpg`;
+        nameArticle[i].innerHTML = data[i].name;
+        priceArticle[i].innerHTML = `${data[i].price} €`;
+        descriptionArticle[i].innerHTML = data[i].description;
+        // console.log(nameArticle)
+    }
+});
