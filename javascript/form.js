217228fd53b2e840c77f1fb8242b3b0f95fd76ce
Aline Leroy <aline.leroy.27@gmail.com>
2021-03-01 13:56:38 +0100

    commencement du formulaire pour enregistrement fetch post

--- javascript/form.js
+++ javascript/form.js
@@ -7,7 +7,7 @@ function displayForm() {
         const divOne = document.querySelector(".container-basket");
 
         divOne.innerHTML = `
-        <form action="" method="post">
+        <form action="" method="post" id="myForm">
             <label for="firstname">firstname:</label>
             <input id="firstname" name="firstname" type="text">
             <label for="lastname">lastname:</label>
@@ -18,9 +18,47 @@ function displayForm() {
             <input id="city" name="city" type="text">
             <label for="email">email:</label>
             <input id="email" name="email" type="text">
-            <input type="submit" value="Valider ma commande"
+            <input type="submit" value="Valider ma commande">
         </form>
         `
     })
 }
 displayForm()
+
+fetch(`http://localhost:3000/api/cameras/order`), {
+    method: "POST",
+    headers: {
+        'Accept': 'app/json',
+        'Content-Type': 'app/json'
+    }, 
+    body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        address: address,
+        city: city,
+        email: email
+    })
+    .then((response) => {
+        if(response.ok) {
+            return response.json();
+        } else {
+            return Error("Error");
+        }
+    })
+
+    .then((data) => {
+        console.log(data)
+
+        const myForm = document.querySelector("#myForm");
+
+        myForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            console.log("yo");
+        })
+    })
+
+    .catch(error => {
+        console.log(error);
+    })
+}
