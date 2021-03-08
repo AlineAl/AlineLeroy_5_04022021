a7ef461c9ff2545d3ef01d1521349f62476bf961
Aline Leroy <aline.leroy.27@gmail.com>
2021-03-08 13:51:54 +0100

    relecture et correction du code

--- javascript/form.js
+++ javascript/form.js
@@ -6,135 +6,140 @@ productItems = JSON.parse(productItems);
 productsBasket.push(productItems);
 
 // form valide ou invalide
-const inputUserFirst = document.querySelector(".form-user:nth-child(1) input");
-const inputUserLast = document.querySelector(".form-user:nth-child(2) input");
-const inputAddress = document.querySelector(".form-user:nth-child(3) input");
-const inputCity = document.querySelector(".form-user:nth-child(4) input");
-const inputMail = document.querySelector(".form-user:nth-child(5) input");
-const imgVerif = document.querySelectorAll(".check-icon-img");
-const span = document.querySelectorAll("span");
+function verifValidInput() {
+    const inputUserFirst = document.querySelector(".form-user:nth-child(1) input");
+    const inputUserLast = document.querySelector(".form-user:nth-child(2) input");
+    const inputAddress = document.querySelector(".form-user:nth-child(3) input");
+    const inputCity = document.querySelector(".form-user:nth-child(4) input");
+    const inputMail = document.querySelector(".form-user:nth-child(5) input");
+    const imgVerif = document.querySelectorAll(".check-icon-img");
+    const span = document.querySelectorAll("span");
 
-inputUserFirst.addEventListener("input", (e) => {
-    if(e.target.value.length >= 3) {
-        imgVerif[0].style.display = "inline";
-        imgVerif[0].src = "./images/check.png";
-        span[0].style.display= "none";
-    } else {
-        imgVerif[0].style.display = "inline";
-        imgVerif[0].src = "./images/error.png";
-        span[0].style.display= "inline";
-    }
-})
+    inputUserFirst.addEventListener("input", (e) => {
+        if(e.target.value.length >= 3) {
+            imgVerif[0].style.display = "inline";
+            imgVerif[0].src = "./images/check.png";
+            span[0].style.display= "none";
+        } else {
+            imgVerif[0].style.display = "inline";
+            imgVerif[0].src = "./images/error.png";
+            span[0].style.display= "inline";
+        }
+    })
 
-inputUserLast.addEventListener("input", (e) => {
-    if(e.target.value.length >= 3) {
-        imgVerif[1].style.display = "inline";
-        imgVerif[1].src = "./images/check.png";
-        span[1].style.display= "none";
-    } else {
-        imgVerif[1].style.display = "inline";
-        imgVerif[1].src = "./images/error.png";
-        span[1].style.display= "inline";
-    }
-})
+    inputUserLast.addEventListener("input", (e) => {
+        if(e.target.value.length >= 3) {
+            imgVerif[1].style.display = "inline";
+            imgVerif[1].src = "./images/check.png";
+            span[1].style.display= "none";
+        } else {
+            imgVerif[1].style.display = "inline";
+            imgVerif[1].src = "./images/error.png";
+            span[1].style.display= "inline";
+        }
+    })
 
-inputAddress.addEventListener("input", (e) => {
-    const regexAddress = /([0-9]*) ?([a-zA-Z,\. ]*) ?([0-9]{5}) ?([a-zA-Z]*)/;
+    inputAddress.addEventListener("input", (e) => {
+        const regexAddress = /([0-9]*) ?([a-zA-Z,\. ]*) ?([0-9]{5}) ?([a-zA-Z]*)/;
 
-    if(e.target.value.search(regexAddress) === 0) {
-        imgVerif[2].style.display = "inline";
-        imgVerif[2].src = "./images/check.png";
-        span[2].style.display= "none";
-    } else if(e.target.value.search(regexAddress) === -1) {
-        imgVerif[2].style.display = "inline";
-        imgVerif[2].src = "./images/error.png";
-        span[2].style.display= "inline";
-    }
-})
+        if(e.target.value.search(regexAddress) === 0) {
+            imgVerif[2].style.display = "inline";
+            imgVerif[2].src = "./images/check.png";
+            span[2].style.display= "none";
+        } else if(e.target.value.search(regexAddress) === -1) {
+            imgVerif[2].style.display = "inline";
+            imgVerif[2].src = "./images/error.png";
+            span[2].style.display= "inline";
+        }
+    })
 
-inputCity.addEventListener("input", (e) => {
-    const regexCity = /^[a-zA-Z]+(?:(?:\\s+|-)[a-zA-Z]+)*$/;
+    inputCity.addEventListener("input", (e) => {
+        const regexCity = /^[a-zA-Z]+(?:(?:\\s+|-)[a-zA-Z]+)*$/;
 
-    if(e.target.value.search(regexCity) === 0) {
-        imgVerif[3].style.display = "inline";
-        imgVerif[3].src = "./images/check.png";
-        span[3].style.display= "none";
-    } else if(e.target.value.search(regexCity) === -1) {
-        imgVerif[3].style.display = "inline";
-        imgVerif[3].src = "./images/error.png";
-        span[3].style.display= "inline";
-    }
-})
+        if(e.target.value.search(regexCity) === 0) {
+            imgVerif[3].style.display = "inline";
+            imgVerif[3].src = "./images/check.png";
+            span[3].style.display= "none";
+        } else if(e.target.value.search(regexCity) === -1) {
+            imgVerif[3].style.display = "inline";
+            imgVerif[3].src = "./images/error.png";
+            span[3].style.display= "inline";
+        }
+    })
 
-inputMail.addEventListener("input", (e) => {
-    const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    inputMail.addEventListener("input", (e) => {
+        const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if(e.target.value.search(regexEmail) === 0) {
-        imgVerif[4].style.display = "inline";
-        imgVerif[4].src = "./images/check.png";
-        span[4].style.display= "none";
-    } else if(e.target.value.search(regexEmail) === -1) {
-        imgVerif[4].style.display = "inline";
-        imgVerif[4].src = "./images/error.png";
-        span[4].style.display= "inline";
-    }
-})
-
+        if(e.target.value.search(regexEmail) === 0) {
+            imgVerif[4].style.display = "inline";
+            imgVerif[4].src = "./images/check.png";
+            span[4].style.display= "none";
+        } else if(e.target.value.search(regexEmail) === -1) {
+            imgVerif[4].style.display = "inline";
+            imgVerif[4].src = "./images/error.png";
+            span[4].style.display= "inline";
+        }
+    })
+}
+verifValidInput()
 
 // Je fetch post les données utilisateurs
 // contact (information de contact) / product (id des products en string)
 // requête order = renvoie un order id généré
-const userForm = document.querySelector("#my-form");
+function requestPost() {
+    const userForm = document.querySelector("#my-form");
 
-userForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+    userForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    // console.log("yo");
-    
-    let contact = {
-        firstName: document.querySelector('#firstname').value,
-        lastName: document.querySelector('#lastname').value,
-        address: document.querySelector('#address').value,
-        city: document.querySelector('#city').value,
-        email: document.querySelector('#email').value
-    }
-    // console.log(contact);
-    let products = [];
-
-    productsBasket = productsBasket[0];
-    for(let i = 0; i < productsBasket.length; i++) {
-        products.push(productsBasket[i].object._id);  
-    }
-
-    // console.log(product_id);
-
-    const form = {contact, products};
-    // console.log(form);
-
-    fetch('http://localhost:3000/api/cameras/order', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(form)
-    })
-
-    .then((response) => {
-        return response.json();
+        // console.log("yo");
         
-    })
+        let contact = {
+            firstName: document.querySelector('#firstname').value,
+            lastName: document.querySelector('#lastname').value,
+            address: document.querySelector('#address').value,
+            city: document.querySelector('#city').value,
+            email: document.querySelector('#email').value
+        }
+        // console.log(contact);
+        let products = [];
 
-    .then((data) => {
-        localStorage.setItem("contact", JSON.stringify(form));
-        localStorage.setItem("orderId", JSON.stringify(data.orderId));
-        console.log(form);
-        console.log(data.orderId);
-        const a = document.querySelector('a');
-        window.location.href=`./order.html?orderid=${data.orderId}`;
-    })
+        productsBasket = productsBasket[0];
+        for(let i = 0; i < productsBasket.length; i++) {
+            products.push(productsBasket[i].object._id);  
+        }
 
-    .catch(error => {
-        console.log(error);
-    }) 
-})
+        // console.log(product_id);
+
+        const form = {contact, products};
+        // console.log(form);
+
+        fetch('http://localhost:3000/api/cameras/order', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(form)
+        })
+
+        .then((response) => {
+            return response.json();
+            
+        })
+
+        .then((data) => {
+            localStorage.setItem("contact", JSON.stringify(form));
+            localStorage.setItem("orderId", JSON.stringify(data.orderId));
+            console.log(form);
+            console.log(data.orderId);
+            window.location.href=`./order.html?orderid=${data.orderId}`;
+        })
+
+        .catch(error => {
+            console.log(error);
+        }) 
+    })
+}
+requestPost()
+
--- javascript/order.js
+++ javascript/order.js
@@ -2,32 +2,37 @@
 const orderId = JSON.parse(localStorage.getItem("orderId"));
 const total = JSON.parse(localStorage.getItem("total"));
 
-// Affichage des données sur la page de confirmation
-const div = document.querySelector('div');
+function displayTotalAndOrderNumber() {
+    // Affichage des données sur la page de confirmation
+    const div = document.querySelector('div');
 
-if(total === null && orderId === null) {
+    if(total === null && orderId === null) {
 
-    const pNull = document.createElement('p');
-    pNull.setAttribute("class", "p-null");
-    pNull.innerText = `Votre panier est vide 😅`;
-    div.prepend(pNull);
-    
-} else {
+        const pNull = document.createElement('p');
+        pNull.setAttribute("class", "p-null");
+        pNull.innerText = `Votre panier est vide 😅`;
+        div.prepend(pNull);
+        
+    } else {
 
-    const h2 = document.createElement("h2");
-    h2.innerText = "Félicitations ! 🥳"
-    div.appendChild(h2);
+        const h2 = document.createElement("h2");
+        h2.innerText = "Félicitations ! 🥳"
+        div.appendChild(h2);
 
-    const pTotal = document.createElement('p');
-    pTotal.setAttribute("class", "p-total-order");
-    pTotal.innerText = `Nous vous remercions pour votre commande d'un total de ${total} €`;
-    div.appendChild(pTotal);
+        const pTotal = document.createElement('p');
+        pTotal.setAttribute("class", "p-total-order");
+        pTotal.innerText = `Nous vous remercions pour votre commande d'un total de ${total} €`;
+        div.appendChild(pTotal);
 
-    const pOrderId = document.createElement('p');
-    pOrderId.setAttribute("class", "p-order-id");
-    pOrderId.innerText = `Voici votre numéro de commande: ${orderId}`;
-    div.appendChild(pOrderId);
+        const pOrderId = document.createElement('p');
+        pOrderId.setAttribute("class", "p-order-id");
+        pOrderId.innerText = `Voici votre numéro de commande: ${orderId}`;
+        div.appendChild(pOrderId);
 
-    localStorage.clear(); 
+        localStorage.clear(); 
+    }
 }
+displayTotalAndOrderNumber()
+
+
 
